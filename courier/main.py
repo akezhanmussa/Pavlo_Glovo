9c967ad4dcf522bf95961776593e165b558a534a
DaDsoul <ake.mussa@gmail.com>
2020-05-13 21:58:30 +0600

    add keyboard menu

--- courier/main.py
+++ courier/main.py
@@ -1,22 +1,23 @@
 import telebot 
 from constants import Constants
+from controller.Controller import Controller
+from telegram.ext import Updater
 
 
-
-bot = telebot.TeleBot(Constants.BOT_TOKEN.value)
-
-
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
-
-
-bot.polling()
-
-# if __name__ == "__main__":
+if __name__ == "__main__":
+    '''
+    This class, which employs the telegram.ext.Dispatcher, 
+    provides a frontend to telegram.Bot to the programmer, 
+    so they can focus on coding the bot. Its purpose is to 
+    receive the updates from Telegram and to deliver them to said dispatcher
+    '''
+    updater = Updater(token=Constants.BOT_TOKEN.value, use_context=True)
+    controller = Controller(updater.dispatcher)
     
-
-
-#     pass
+    # Starts polling updates from Telegram.
+    updater.start_polling(poll_interval=1)
+    # Blocks until one of the signals are received and stops the updater.
+    updater.idle()
+    pass
 
     
---
+++ courier/view/view.py
@@ -0,0 +1,47 @@
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup,ReplyKeyboardMarkup,KeyboardButton
+
+
+class LogMenuView: 
+    """
+    TestView class:
+        the main responsibility of this class is 'sending messages of each state in bot'
+        and also 'formatting of messages', etc.
+    """
+    
+    def __init__(self):
+        
+        self.button_list = [
+            InlineKeyboardButton("Начать Опрос", callback_data="switch_to_registration"),
+            InlineKeyboardButton("Назад", callback_data="back_to_main_menu")
+        ]
+
+    def show_keyboard_menu(self, update, context):
+        chat_id = update.message.chat.id
+        #telegram.KeyboardButton
+        custom_keyboard = [[KeyboardButton('Регистрация'), KeyboardButton('Войти')], 
+                   [            KeyboardButton('Инфо')]]
+        reply_markup = ReplyKeyboardMarkup(custom_keyboard, resize_keyboard=True,one_time_keyboard=True)
+        context.bot.send_message(chat_id=chat_id, 
+                        text="Добро пожаловать в систему курьеров!", 
+                        reply_markup=reply_markup)
+
+    def send_menu(self, update, context):
+        chat_id = update.message.chat.id
+        reply_markup = InlineKeyboardMarkup(self.build_log_menu(self.button_list, n_cols=1))
+        context.bot.send_message(chat_id = chat_id, text = "Чтобы начать проходить опрос, нажмите Начать Опрос, если вы передумали нажмите Выйти", reply_markup=reply_markup)
+
+    def send_message(self, update, context, message = "Hi"):
+        chat_id = update.message.chat.id
+        context.bot.send_message(chat_id=chat_id, text=message)
+
+    def build_log_menu(self, buttons, n_cols, header_buttons=None, footer_buttons=None):
+        menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
+        if header_buttons:
+            menu.insert(0, [header_buttons])
+        if footer_buttons:
+            menu.append([footer_buttons])
+        return menu
+
+    def call_registration(self): 
+        print("Registration was called")
